7cce7d8cc18a34b1dbe93de058d69ee8e3f9e9e5
honeyypie <145855947+honeyypie@users.noreply.github.com>
2023-09-30 18:38:02 +0300

    ybbvip
    shlakoblock

---
+++ 2Urokvvedeniepython.py
@@ -0,0 +1,19 @@
+#num1 = 123
+#non2 = None
+#array = [1,2,3,4,5] 
+#flor1 = 2.5
+#str1 = "lox"
+#print(type(num1))
+#print(type(non2))
+#print(type(shlakoblock))
+#print(type(flor1))
+#print(type(str1))
+list1 = "Hello world A Sports It's in a Game"
+#print(list1 * 10 + "\t")
+str3="a"
+str2="b"
+print(str2 + str3)
+print(len[str2],len(list1))
+for i in list1:
+    if str2:
+        print(i)
